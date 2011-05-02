a00f864eaabc70b890c3638e091667d401aec9bb
Andrew Vos <andrew.vos@gmail.com>
2011-05-02 14:55:43 +0100

    roman numerals can now be created from decimals

--- lib/roman-numerals.rb
+++ lib/roman-numerals.rb
@@ -1,2 +1,31 @@
 class RomanNumerals
+  @base_digits = {
+    1    => 'I',
+    4    => 'IV',
+    5    => 'V',
+    9    => 'IX',
+    10   => 'X',
+    40   => 'XL',
+    50   => 'L',
+    90   => 'XC',
+    100  => 'C',
+    400  => 'CD',
+    500  => 'D',
+    900  => 'CM',
+    1000 => 'M'
+  }
+  def self.to_roman(value)
+    if @base_digits.keys.include? value
+      @base_digits[value]
+    else
+      result = ''
+      @base_digits.keys.reverse.each do |decimal|
+        while value >= decimal
+          value -= decimal
+          result += @base_digits[decimal]
+        end
+      end
+      result
+    end
+  end
 end
--- spec/roman-numerals_spec.rb
+++ spec/roman-numerals_spec.rb
@@ -1,6 +1,29 @@
 require 'helper'
 
 describe RomanNumerals do
-  it "passes a test" do
+  describe ".to_roman" do
+    {
+      1    => 'I',
+      4    => 'IV',
+      5    => 'V',
+      9    => 'IX',
+      10   => 'X',
+      40   => 'XL',
+      50   => 'L',
+      90   => 'XC',
+      100  => 'C',
+      400  => 'CD',
+      500  => 'D',
+      900  => 'CM',
+      1000 => 'M'
+    }.each do |decimal, roman_numeral|
+      it "converts the decimal value #{decimal} to the roman numeral #{roman_numeral}" do
+        RomanNumerals.to_roman(decimal).should == roman_numeral
+      end
+    end
+    it "converts larger decimals" do
+      RomanNumerals.to_roman(23234).should == 'MMMMMMMMMMMMMMMMMMMMMMMCCXXXIV'
+      RomanNumerals.to_roman(42).should == 'XLII'
+    end
   end
 end
